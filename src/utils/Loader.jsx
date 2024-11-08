1d1755f83a97ce108d282f00dbab643cae15fbdb
Shivanshsri15 <167137978+Shivanshsri15@users.noreply.github.com>
2024-11-08 15:27:38 +0530

    Add files via upload

---
+++ src/utils/Loader.jsx
@@ -0,0 +1,11 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+function Loader() {
+    return (<div className='text-center'>
+      
+        <Spinner animation = "grow" />
+    </div> 
+)}
+
+export default Loader;
+
