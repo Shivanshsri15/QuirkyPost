0a5a16e37babc5a1a8cb0f42cab752e23768d0c5
Shivanshsri15 <167137978+Shivanshsri15@users.noreply.github.com>
2024-12-09 00:08:54 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { authUser, logOut, registerUser } from '../controllers/userController.js'
-
-const router = express.Router()
-
-router.route("/").post(registerUser);
-router.route("/auth").post(authUser);
-router.post("/logout", logOut)
-export default router;
