cce4e637429241c824f7c3333d08abfb9772a494
Shivanshsri15 <167137978+Shivanshsri15@users.noreply.github.com>
2024-12-27 23:17:36 +0530

    Delete src directory

--- src/components/ProgressBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ProgressBar } from "react-bootstrap";
-const ProgressBarPro = (props) => {
-  return <ProgressBar animated label={`${props.val}%`} now={props.val} variant={props.var } />;
-};
-
-export default ProgressBarPro;
