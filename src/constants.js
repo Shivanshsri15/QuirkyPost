19dd689d337dfd1e12985b8738f5bc3f589884fe
Shivanshsri15 <167137978+Shivanshsri15@users.noreply.github.com>
2025-01-09 22:43:12 +0530

    Delete src directory

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "";
-export const USERS_URL = "/quirky/users";
-export const PFUPLOAD = "/quirky/pfuploads"
-export const COVERUPLOAD = "/quirky/coveruploads"
-export const BLOGS_URL = "/quirky/blogs"
